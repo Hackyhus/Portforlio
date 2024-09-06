5a7b4d0907b2fecc43e4e929abb3d31079df640a
Hassan Abdulhamid <hassanabdulhamid603@gmail.com>
2024-09-06 07:18:37 +0100

    second

---
+++ src/sections/Hero/Hero.jsx
@@ -0,0 +1,71 @@
+import styles from "./HeroStyles.module.css";
+import heroImg from "../../assets/Design uten navn.png";
+import sun from "../../assets/sun.svg";
+import moon from "../../assets/moon.svg";
+import twitterLight from "../../assets/twitter-light.svg";
+import twitterDark from "../../assets/twitter-dark.svg";
+import githubLight from "../../assets/github-light.svg";
+import githubDark from "../../assets/github-dark.svg";
+import linkedinLight from "../../assets/linkedin-light.svg";
+import linkedinDark from "../../assets/linkedin-dark.svg";
+import CV from "../../assets/CV.pdf";
+import { useTheme } from "../../common/ThemeContext";
+
+function Hero() {
+  const { theme, toggleTheme } = useTheme();
+
+  const themeIcon = theme === "light" ? sun : moon;
+  const twitterIcon = theme === "light" ? twitterLight : twitterDark;
+  const githubIcon = theme === "light" ? githubLight : githubDark;
+  const linkedinIcon = theme === "light" ? linkedinLight : linkedinDark;
+
+  return (
+    <section id="hero" className={styles.container}>
+      <div className={styles.colorModeContainer}>
+        <img
+          src={heroImg}
+          className={styles.hero}
+          alt="Profile picture of Hassan Abdulhamid"
+        />
+        <img
+          className={styles.colorMode}
+          src={themeIcon}
+          alt="Color mode icon"
+          onClick={toggleTheme}
+        />
+      </div>
+      <div className={styles.info}>
+        <h1>
+          HASSAN
+          <br />
+          ABDULHAMID
+        </h1>
+        <h2>Front-End Developer</h2>
+        <span>
+          <a href="https://x.com/HassiumAB" target="_blank">
+            <img src={twitterIcon} alt="Twitter icon" />
+          </a>
+          <a href="https://github.com/Hackyhus" target="_blank">
+            <img src={githubIcon} alt="Github icon" />
+          </a>
+          <a
+            href="https://linkedin.com/in/hassan-abdulhamid-668a61326"
+            target="_blank"
+          >
+            <img src={linkedinIcon} alt="Linkedin icon" />
+          </a>
+        </span>
+        <p className={styles.description}>
+          Highly motivated and detail-oriented frontend developer with a passion
+          for crafting exceptional user experiences. Proficient in HTML, CSS,
+          JavaScript, and frameworks like React and Angular.
+        </p>
+        <a href={CV} download>
+          <button className="hover">Resume</button>
+        </a>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
---
+++ src/sections/Projects/Projects.jsx
@@ -0,0 +1,42 @@
+import styles from "./ProjectsStyles.module.css";
+import viber from "../../assets/logo_icon.png";
+import calc from "../../assets/calc-logo.jpg";
+import hipster from "../../assets/hipster.png";
+import fitLift from "../../assets/counter-app.png";
+import ProjectCard from "../../common/ProjectCard";
+
+function Projects() {
+  return (
+    <section id="projects" className={styles.container}>
+      <h1 className="sectionTitle">PROJECTS</h1>
+      <div className={styles.projectsContainer}>
+        <ProjectCard
+          src={viber}
+          link="https://github.com/Hackyhus/Chatapp.git"
+          h3="Chatapp"
+          p="A Chatting App"
+        />
+        <ProjectCard
+          src={calc}
+          link="https://github.com/Hackyhus/calculator.git"
+          h3="Calculator"
+          p="A Calculator"
+        />
+        {/* <ProjectCard
+          src={hipster}
+          link="https://github.com/Hackyhus/"
+          h3="Livestockapp"
+          p="App that keeps livestock records"
+        /> */}
+        <ProjectCard
+          src={fitLift}
+          link="https://github.com/Hackyhus/Counter.git"
+          h3="Counter"
+          p="A counter app"
+        />
+      </div>
+    </section>
+  );
+}
+
+export default Projects;
